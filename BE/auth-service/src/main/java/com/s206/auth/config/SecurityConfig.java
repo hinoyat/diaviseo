7f4f3e5a659388896470ba741721c878b377c986
hinoyat <hhwj2270@gmail.com>
2025-04-24 13:45:58 +0900

    [BE] Feat: SpringSecurity session 비활성화
    - MSA 환경에서 서비스끼리 세션을 공유하기 어려줘 JWT 사용하기에 비활성화

--- BE/auth-service/src/main/java/com/s206/auth/config/SecurityConfig.java
+++ BE/auth-service/src/main/java/com/s206/auth/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.s206.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +13,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // API 서버 csrf 비활성화
                 .csrf(csrf -> csrf.disable())
+
+                // JWT 사용으로 세션 사용 X
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 개발 중 포인트 모두 허용
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/oauth2/**",                // OAuth 콜백
