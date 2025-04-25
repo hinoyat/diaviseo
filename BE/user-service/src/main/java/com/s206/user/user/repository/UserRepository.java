0fe2e9f03035598cdc275d54e8749eeb95e0d254
hinoyat <hhwj2270@gmail.com>
2025-04-25 16:39:07 +0900

    [BE] Feat: User Repository 추가

---
+++ BE/user-service/src/main/java/com/s206/user/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.s206.user.user.repository;
+
+import com.s206.user.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
