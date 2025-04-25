55685752e403cce820387cd7a3bb010aa0ae0172
임채현 <chi_927@naver.com>
2025-04-26 01:10:12 +0900

    [FE] Feat: 상단 상태바 배경 투명, ui 공통으로 빼기

--- FE/mobile/src/main/java/com/example/diaviseo/MainActivity.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.diaviseo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.diaviseo.ui.signup.SignupScreen
+import com.example.diaviseo.ui.signup.SignupNavGraph
+import com.example.diaviseo.ui.components.TransparentStatusBar
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        TransparentStatusBar(window)
+
         setContent {
-            SignupScreen()
+            val navController = rememberNavController()
+            SignupNavGraph(navController)
         }
     }
 }
