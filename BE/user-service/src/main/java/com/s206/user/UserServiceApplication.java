63b52fc1c8a49b9d0768a3553e38337f64816746
hinoyat <hhwj2270@gmail.com>
2025-04-25 16:37:15 +0900

    [BE] Feat: user-service common-module 적용

--- BE/user-service/src/main/java/com/s206/user/UserServiceApplication.java
+++ BE/user-service/src/main/java/com/s206/user/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.s206.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.s206.user", "com.s206.common"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
