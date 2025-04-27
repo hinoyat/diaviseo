2420a95fb6cf4b68a4ca868dedb4fa275da8a738
daeun <danivalen@naver.com>
2025-04-27 10:39:16 +0900

    [FE] CHORE : 의존성 중복 코드 제거

--- FE/mobile/build.gradle.kts
+++ FE/mobile/build.gradle.kts
@@ -69,5 +69,4 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
