464506301330e0826c14bf7f70cf4c76119a0d23
Hinoyat <hhwj2270@gmail.com>
2025-04-27 16:47:30 +0900

    [BE] Feat: WebClient -> user-service에 유저 존재여부 확인 추가

---
+++ BE/auth-service/src/main/java/com/s206/auth/client/UserServiceClient.java
@@ -0,0 +1,38 @@
+package com.s206.auth.client;
+
+import com.s206.auth.client.dto.UserExistResponse;
+import com.s206.common.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceClient {
+
+    private final WebClient.Builder loadBalancedWebClientBuilder;
+
+    private static final String USER_SERVICE_BASE_URL = "http://user-service";
+
+
+    public boolean checkUserExists(String email, String provider) {
+        ResponseDto<UserExistResponse> response = loadBalancedWebClientBuilder
+                .baseUrl(USER_SERVICE_BASE_URL)
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/users/exist")
+                        .queryParam("email", email)
+                        .queryParam("provider", provider)
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ResponseDto<UserExistResponse>>() {})
+                .block(); // 동기 호출
+
+        return response != null && response.getData() != null && response.getData().isExists();
+    }
+
+}
