a091f2ea091fb1c8c55902904616f9d6bca2baf9
이다은 <danivalen@naver.com>
2025-04-28 11:31:16 +0900

    [FE] Design : 상단 System UI 색 변경 (흰 -> 검)

--- FE/mobile/src/main/java/com/example/diaviseo/MainActivity.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.diaviseo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.SideEffect
 
 import com.example.diaviseo.ui.signup.SignupNavGraph
 import com.example.diaviseo.ui.components.TransparentStatusBar
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.graphics.Color
 
 import com.example.diaviseo.ui.main.MainScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +20,20 @@ class MainActivity : ComponentActivity() {
 //        TransparentStatusBar(window)
 
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val navController = rememberNavController()
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.Transparent,
+                    darkIcons = true // 글씨와 아이콘을 검은색으로
+                )
+            }
             TransparentStatusBar(window) // setContent {} 안에서 호출
             // 회원가입 & 로그인 로직 구현 이후
             // 로그인, 회원가입된 사용자 -> MainScreen으로
             // 회원가입해야하는 신규 유저 -> SignupNavGraph로 이동하도록 수정 필요
 
-            MainScreen()
-            val navController = rememberNavController()
+            MainScreen() // 추후에 로직 구현 이후 주석 제거
             SignupNavGraph(navController)
         }
     }
