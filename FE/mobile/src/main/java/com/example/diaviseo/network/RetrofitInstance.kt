7fa7f606eb7db8ae0220da2d79ab0a32fe5e6296
chaehyeon <chi_927@naver.com>
2025-04-30 13:18:08 +0900

    [FE] Feat: header에 토큰 필요한 retrofit 형식 추가

--- FE/mobile/src/main/java/com/example/diaviseo/network/RetrofitInstance.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/network/RetrofitInstance.kt
@@ -1,18 +1,51 @@
 package com.example.diaviseo.network
 
+import android.util.Log
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.example.diaviseo.BuildConfig
+import okhttp3.OkHttpClient
 
 object RetrofitInstance {
 
     // RetrofitInstance.api.어떤api요청() 형식으로 사용 가능
-    val api: AuthApiService by lazy {
+    val authApiService: AuthApiService by lazy {
         Retrofit.Builder()
-//            .baseUrl(BuildConfig.BASE_URL)
-            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(AuthApiService::class.java)
     }
+
+    private val retro: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)// 공통 base URL
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer token")  // 공통 헤더
+                    .build()
+                val response = chain.proceed(newRequest)
+
+//                // 401 오류가 나면 자동으로 토큰 갱신 요청 => 우왕 나중에 써야지
+//                if (response.code == 401) {
+//                    val newToken = refreshAuthToken() // 새로운 토큰을 갱신하는 함수
+//                    val newRequest = request.newBuilder()
+//                        .addHeader("Authorization", "Bearer $newToken")
+//                        .build()
+//
+//                    // 새 토큰으로 요청 다시 보내기
+//                    return@addInterceptor chain.proceed(newRequest)
+
+                Log.d("Network", "Response Code: ${response.code}")
+                response
+            }.build())
+            .build()
+    }
+
+    // 기능별 API 서비스 (아래와 같은 형식으로)
+//    val homeApiService: HomeApiService by lazy {
+//        retro.create(HomeApiService::class.java)
+//    }
+    // val homeResponse = homeApiService.getHomeData() 라고 사용하면 됩니다
 }
