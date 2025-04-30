00cbb72c0ffcda85d6659d9e1147552c7803d52f
chaehyeon <chi_927@naver.com>
2025-04-30 13:19:06 +0900

    [FE] Feat: 같은 viewmodel 공유 위해 navigation 묶기

--- FE/mobile/src/main/java/com/example/diaviseo/ui/signup/SignupNavGraph.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/signup/SignupNavGraph.kt
@@ -4,11 +4,17 @@ package com.example.diaviseo.ui.signup
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
+import androidx.navigation.navigation
 
 fun NavGraphBuilder.signupNavGraph(navController: NavHostController) {
-    composable("signup") { SignupScreen(navController) }
-    composable("inputName") { InputNameScreen(navController) }
-    composable("selectGender") { SelectGenderScreen(navController) }
-    composable("inputBirth") { InputBirthScreen(navController) }
-    composable("phoneAuth") { PhoneAuthScreen(navController) }   // 소셜 로그인 이후 휴대폰 인증
+    navigation(
+        startDestination = "signup",
+        route = "signupGraph"
+    ) {
+        composable("signup") { SignupScreen(navController) }
+        composable("inputName") { InputNameScreen(navController) }
+        composable("selectGender") { SelectGenderScreen(navController) }
+        composable("inputBirth") { InputBirthScreen(navController) }
+        composable("phoneAuth") { PhoneAuthScreen(navController) }   // 소셜 로그인 이후 휴대폰 인증
+    }
 }
