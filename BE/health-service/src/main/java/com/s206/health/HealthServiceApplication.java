8932acff71831d26c04d33b456aa023139d6952c
Kang Hyeon Ho <hhwj2270@gmail.com>
2025-04-30 12:07:19 +0900

    [BE] Feat: common-module 의존성 추가

--- BE/health-service/src/main/java/com/s206/health/HealthServiceApplication.java
+++ BE/health-service/src/main/java/com/s206/health/HealthServiceApplication.java
@@ -3,9 +3,11 @@ package com.s206.health;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.s206.health", "com.s206.common"})
 public class HealthServiceApplication {
 
 	public static void main(String[] args) {
