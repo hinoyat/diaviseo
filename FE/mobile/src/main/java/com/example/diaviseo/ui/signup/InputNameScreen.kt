7b87ecfd534ba99770f16991a639a143f3efe550
chaehyeon <chi_927@naver.com>
2025-04-30 13:19:43 +0900

    [FE] Test: 같은 viewmodel 공유를 위한 양식 추가

--- FE/mobile/src/main/java/com/example/diaviseo/ui/signup/InputNameScreen.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/signup/InputNameScreen.kt
@@ -1,10 +1,21 @@
 package com.example.diaviseo.ui.signup
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.compose.runtime.remember
+import com.example.diaviseo.viewmodel.AuthViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+
+@SuppressLint("UnrememberedGetBackStackEntry")
 @Composable
 fun InputNameScreen(navController: NavController) {
-    Text(text = "여긴 InputNameScreen")
+    val parentEntry = remember(navController) {
+        navController.getBackStackEntry("signupGraph")
+    }
+    val authViewModel: AuthViewModel = viewModel(parentEntry)
+
+    // 이 authViewModel은 SignupScreen, SelectGenderScreen과 **공유됨**!
 }
