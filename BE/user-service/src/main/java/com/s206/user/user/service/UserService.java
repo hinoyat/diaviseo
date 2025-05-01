c5cbad05fe0b602928caa0bb94a8f79e569cbab0
Hinoyat <hhwj2270@gmail.com>
2025-05-01 19:09:31 +0900

    [BE] Feat: 회원가입, 수정, 탈퇴, 존재 확인 로직 통합 구현

--- BE/user-service/src/main/java/com/s206/user/user/service/UserService.java
+++ BE/user-service/src/main/java/com/s206/user/user/service/UserService.java
@@ -1,15 +1,20 @@
 package com.s206.user.user.service;
 
+import com.s206.common.exception.types.BadRequestException;
 import com.s206.common.exception.types.ConflictException;
 import com.s206.common.exception.types.NotFoundException;
 import com.s206.user.user.dto.request.UserCreateRequest;
+import com.s206.user.user.dto.request.UserUpdateRequest;
 import com.s206.user.user.dto.response.UserDetailResponse;
 import com.s206.user.user.dto.response.UserExistResponse;
 import com.s206.user.user.entity.User;
 import com.s206.user.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -18,46 +23,117 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-
-    public UserDetailResponse createUser(UserCreateRequest userCreateRequest) {
-        if (userRepository.existsByEmail(userCreateRequest.getEmail())) {
+    @Transactional
+    public UserDetailResponse createUser(UserCreateRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
             throw new ConflictException("이미 가입된 이메일입니다.");
         }
+
+        if (userRepository.existsByPhone(request.getPhone())) {
+            throw new ConflictException("이미 등록된 전화번호입니다.");
+        }
+
         User user = User.builder()
-                .name(userCreateRequest.getName())
-                .nickname(userCreateRequest.getNickname())
-                .gender(userCreateRequest.getGender())
-                .birthday(userCreateRequest.getBirthday())
-                .phone(userCreateRequest.getPhone())
-                .email(userCreateRequest.getEmail())
-                .provider(userCreateRequest.getProvider())
-                .consentPersonal(userCreateRequest.getConsentPersonal())
-                .locationPersonal(userCreateRequest.getLocationPersonal())
+                .name(request.getName())
+                .nickname(request.getNickname())
+                .gender(request.getGender())
+                .height(request.getHeight())
+                .weight(request.getWeight())
+                .birthday(request.getBirthday())
+                .phone(request.getPhone())
+                .email(request.getEmail())
+                .provider(request.getProvider())
+                .consentPersonal(request.getConsentPersonal())
+                .locationPersonal(request.getLocationPersonal())
                 .build();
+
         userRepository.save(user);
+        return UserDetailResponse.toDto(user);
+    }
+
+
+    public UserDetailResponse getUserByUserId(Integer userId) {
+        User user = validUser(userId);
 
         return UserDetailResponse.toDto(user);
     }
 
-    public UserDetailResponse getUserByEmail(String email) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException("해당 이메일로 가입된 유저가 없습니다."));
+    @Transactional
+    public UserDetailResponse updateUser(Integer userId, UserUpdateRequest request) {
+        User user = validUser(userId);
+
+        if (request.getNickname() != null) {
+            if (request.getNickname().isBlank()) {
+                throw new BadRequestException("닉네임은 비어있을 수 없습니다.");
+            }
+            user.updateNickname(request.getNickname());
+        }
+
+        if (request.getPhone() != null) {
+            if (request.getPhone().isBlank()) {
+                throw new BadRequestException("전화번호는 비어있을 수 없습니다.");
+            }
+            user.updatePhone(request.getPhone());
+        }
+
+        if (request.getHeight() != null) {
+            if (request.getHeight().compareTo(BigDecimal.ZERO) <= 0) {
+                throw new BadRequestException("키는 0보다 커야 합니다.");
+            }
+            user.updateHeight(request.getHeight());
+        }
+
+        if (request.getWeight() != null) {
+            if (request.getWeight().compareTo(BigDecimal.ZERO) <= 0) {
+                throw new BadRequestException("몸무게는 0보다 커야 합니다.");
+            }
+            user.updateWeight(request.getWeight());
+        }
+
+        if (request.getBirthday() != null) {
+            if (request.getBirthday().isAfter(LocalDate.now())) {
+                throw new BadRequestException("생일은 미래일 수 없습니다.");
+            }
+            user.updateBirthday(request.getBirthday());
+        }
+
+        if (request.getNotificationEnabled() != null) {
+            user.updateNotificationEnabled(request.getNotificationEnabled());
+        }
 
         return UserDetailResponse.toDto(user);
+    }
+
+    @Transactional
+    public void deleteUser(Integer userId) {
+        User user = validUser(userId);
+        user.delete();
+        userRepository.save(user);
     }
 
 
     public UserExistResponse existsByEmail(String email, String provider) {
 
-        Optional<User> Optionaluser = userRepository.findByEmailAndProvider(email, provider);
+        Optional<User> optionalUser = userRepository.findByEmailAndProvider(email, provider);
 
-        if (Optionaluser.isPresent()) {
-            User user = Optionaluser.get();
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
             return UserExistResponse.toDto(user, true);
         } else {
             return UserExistResponse.toDto(false);
         }
 
+    }
+
+    private User validUser(Integer userId) {
+        User user = userRepository.findUserByUserId(userId)
+                .orElseThrow(() -> new NotFoundException("해당 유저를 찾을 수 없습니다."));
+
+        if (user.getIsDeleted()) {
+            throw new BadRequestException("이미 탈퇴한 회원입니다.");
+        }
+
+        return user;
     }
 
 }
