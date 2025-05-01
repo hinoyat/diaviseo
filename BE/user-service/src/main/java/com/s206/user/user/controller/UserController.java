ad9da03e6f67482d226130b918daa54b4e55cdd9
Hinoyat <hhwj2270@gmail.com>
2025-05-01 19:09:50 +0900

    [BE] Feat: 회원가입, 조회, 수정, 탈퇴, 존재 확인 컨트롤러 구현

--- BE/user-service/src/main/java/com/s206/user/user/controller/UserController.java
+++ BE/user-service/src/main/java/com/s206/user/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.s206.user.user.controller;
 
 import com.s206.common.dto.ResponseDto;
+import com.s206.user.user.dto.request.PhoneVerificationRequest;
 import com.s206.user.user.dto.request.UserCreateRequest;
+import com.s206.user.user.dto.request.UserUpdateRequest;
 import com.s206.user.user.dto.response.UserDetailResponse;
 import com.s206.user.user.dto.response.UserExistResponse;
 import com.s206.user.user.service.UserService;
@@ -19,7 +21,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/signup")
     public ResponseEntity<ResponseDto<UserDetailResponse>> createUser(
             @RequestBody UserCreateRequest userCreateRequest
     ) {
@@ -29,12 +31,15 @@ public class UserController {
         return ResponseEntity.ok(ResponseDto.success(HttpStatus.CREATED, "회원가입 성공", userDetailResponse));
     }
 
-    @GetMapping("/email/{email}")
-    public ResponseEntity<ResponseDto<UserDetailResponse>> getUserByEmail(@PathVariable String email) {
-        UserDetailResponse user = userService.getUserByEmail(email);
-        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK, "회원 조회 성공", user));
+    @GetMapping("/me")
+    public ResponseEntity<ResponseDto<UserDetailResponse>> getUserByEmail(
+            @RequestHeader("X-USER-ID") Integer userId) {
+        log.info("Getting user by userId: {}", userId);
+        UserDetailResponse user = userService.getUserByUserId(userId);
+        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK, "내 정보 조회 성공", user));
     }
 
+    // Auth-Service 통신 API
     @GetMapping("/exist")
     public ResponseEntity<ResponseDto<UserExistResponse>> checkUserExists(
             @RequestParam("email") String email,
@@ -44,4 +49,24 @@ public class UserController {
         log.info("exists: {}", response.toString());
         return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK, "회원 존재 조회 성공", response));
     }
+
+    @PutMapping
+    public ResponseEntity<ResponseDto<UserDetailResponse>> updateUser(
+            @RequestHeader("X-USER-ID") Integer userId,
+            @RequestBody UserUpdateRequest request
+    ) {
+        log.info("Updating user (userId={}): {}", userId, request.toString());
+        UserDetailResponse updated = userService.updateUser(userId, request);
+        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK, "회원정보 수정 성공", updated));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ResponseDto<Void>> deleteUser(
+            @RequestHeader("X-USER-ID") Integer userId
+    ) {
+        log.info("Deleting user with id: {}", userId);
+        userService.deleteUser(userId);
+        return ResponseEntity.ok(ResponseDto.success(HttpStatus.OK, "회원 탈퇴 처리 완료"));
+    }
+
 }
