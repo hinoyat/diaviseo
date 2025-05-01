4bd809c8ca838db0a5fffb414e12e3f47953213f
임채현 <chi_927@naver.com>
2025-05-01 23:13:15 +0900

    [FE] Feat: mainscreen 에서 환영문구

--- FE/mobile/src/main/java/com/example/diaviseo/ui/main/MainScreen.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/main/MainScreen.kt
@@ -1,31 +1,42 @@
 package com.example.diaviseo.ui.main
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.diaviseo.ui.components.BottomNavigationBar
-import androidx.compose.foundation.layout.padding
+import com.example.diaviseo.ui.main.components.*
 
-import androidx.compose.ui.graphics.Color
 @Composable
 fun MainScreen() {
-    // 화면 이동을 관리해주는 내비게이션 컨트롤러
     val navController = rememberNavController()
+    val userNickname = "김디아" // TODO: ViewModel 등에서 유저 정보 주입
 
-    // Scaffold 를 사용해 하단바 포함한 기본 화면 구성
-    Scaffold (
-        modifier = Modifier.background(Color.White),
-        bottomBar = {
-            BottomNavigationBar(navController = navController)
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController = navController) },
+        containerColor = Color(0xFFDFE9FF)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+//                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    PaddingValues(
+                        start = 16.dp,
+                        top = innerPadding.calculateTopPadding() + 36.dp,
+                        end = 16.dp,
+                        bottom = innerPadding.calculateBottomPadding()
+                    )
+                )
+        ) {
+            MainHeader(userNickname, navController)
         }
-    ){
-        innerPadding ->
-        // 임시로 가운데 텍스트 표시
-        Text (
-            text= "하단바가 잘 나올까요?",
-            modifier = Modifier.padding(innerPadding)
-        )
     }
 }
