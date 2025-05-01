8a13cf0519154cba8e64b4332ea5d3a644ad6e5a
임채현 <chi_927@naver.com>
2025-05-01 14:50:25 +0900

    [FE] Config: datastore 의존성 오타 수정

--- FE/mobile/build.gradle.kts
+++ FE/mobile/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")   // 코루틴
 
-    implementation("androidx.datastore:datastore-preferences:1.1.0")    // datastore
+    implementation("androidx.datastore:datastore-preferences:1.1.5")    // datastore
 
     implementation ("com.google.accompanist:accompanist-systemuicontroller:0.35.0-alpha") // System UI 제어
 
