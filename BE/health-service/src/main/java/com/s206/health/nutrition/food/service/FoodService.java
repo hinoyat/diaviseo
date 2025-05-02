8ed2b2c24386b90e444d61d7b7492c235015b26d
Kang Hyeon Ho <hhwj2270@gmail.com>
2025-05-02 11:56:45 +0900

    [BE] Feat: 음식 상세조회 & 전체 조회 API 추가

---
+++ BE/health-service/src/main/java/com/s206/health/nutrition/food/service/FoodService.java
@@ -0,0 +1,38 @@
+package com.s206.health.nutrition.food.service;
+
+import com.s206.common.exception.types.NotFoundException;
+import com.s206.health.nutrition.favorite.repository.FavoriteFoodRepository;
+import com.s206.health.nutrition.food.dto.response.FoodDetailResponse;
+import com.s206.health.nutrition.food.dto.response.FoodListResponse;
+import com.s206.health.nutrition.food.entity.Food;
+import com.s206.health.nutrition.food.repository.FoodRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FoodService {
+
+    private final FoodRepository foodRepository;
+    private final FavoriteFoodRepository favoriteFoodRepository;
+
+    @Transactional
+    public List<FoodListResponse> getFoodList(Integer userId) {
+        return foodRepository.findAll().stream()
+                .map(food -> FoodListResponse.toDto(food,
+                        favoriteFoodRepository.existsByUserIdAndFoodFoodId(userId, food.getFoodId())))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public FoodDetailResponse getFoodDetail(Integer foodId, Integer userId) {
+        Food food = foodRepository.findById(foodId)
+                .orElseThrow(() -> new NotFoundException("해당 음식을 찾을 수 없습니다."));
+        boolean isFavorite = favoriteFoodRepository.existsByUserIdAndFoodFoodId(userId, foodId);
+        return FoodDetailResponse.toDto(food, isFavorite);
+    }
+}
