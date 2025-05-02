04f28707fd60bc0c001f423aa3ce8d53ac4277b6
Kang Hyeon Ho <hhwj2270@gmail.com>
2025-05-02 13:48:09 +0900

    [BE] Refactor: /api/favorites -> /api/foods/favorites

--- BE/health-service/src/main/java/com/s206/health/nutrition/favorite/controller/FavoriteController.java
+++ BE/health-service/src/main/java/com/s206/health/nutrition/favorite/controller/FavoriteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/favorites")
+@RequestMapping("/api/foods/favorites")
 @RequiredArgsConstructor
 @Slf4j
 public class FavoriteController {
