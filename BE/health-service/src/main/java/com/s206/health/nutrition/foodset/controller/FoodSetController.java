29457093cbd3d1b641ddda7d8cfee6aef98e7315
Kang Hyeon Ho <hhwj2270@gmail.com>
2025-05-02 17:53:23 +0900

    [BE] Feat: FoodSet 기본 Controller & Service & Repository 추가

---
+++ BE/health-service/src/main/java/com/s206/health/nutrition/foodset/controller/FoodSetController.java
@@ -0,0 +1,7 @@
+package com.s206.health.nutrition.foodset.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/foods/foodset")
+public class FoodSetController {
+}
