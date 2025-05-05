3fac6872c6105bd405ebb15ff70ebb0eaf9de578
임채현 <chi_927@naver.com>
2025-05-05 16:50:53 +0900

    [FE] WIP: 목표평가 스크린 생성

---
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/main/GoalScreen.kt
@@ -0,0 +1,17 @@
+package com.example.diaviseo.ui.main
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.example.diaviseo.ui.main.components.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GoalScreen() {
+    Text("여긴 GoalScreen")
+}
