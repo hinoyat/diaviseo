20fad0d68c25ad99b85b16087e087983026f7ad3
Hinoyat <hhwj2270@gmail.com>
2025-05-05 13:31:36 +0900

    [BE] Feat: Daily & Weekly 영양정보 DTO 추가

---
+++ BE/health-service/src/main/java/com/s206/health/nutrition/meal/dto/response/DailyNutritionResponse.java
@@ -0,0 +1,22 @@
+package com.s206.health.nutrition.meal.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class DailyNutritionResponse {
+
+    private LocalDate date;
+
+    private Integer totalCalorie;
+
+    private BigDecimal totalCarbohydrate;
+
+    private BigDecimal totalProtein;
+
+    private BigDecimal totalFat;
+}
