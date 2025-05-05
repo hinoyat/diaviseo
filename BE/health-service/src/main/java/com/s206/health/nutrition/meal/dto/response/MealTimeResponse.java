693c76b5ed0eb40efbac5b42122a26f49ff1f538
Hinoyat <hhwj2270@gmail.com>
2025-05-05 13:31:16 +0900

    [BE] Feat: 식단 응답 Dto 추가

---
+++ BE/health-service/src/main/java/com/s206/health/nutrition/meal/dto/response/MealTimeResponse.java
@@ -0,0 +1,37 @@
+package com.s206.health.nutrition.meal.dto.response;
+
+import com.s206.health.nutrition.meal.entity.MealTime;
+import com.s206.health.nutrition.meal.entity.MealType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class MealTimeResponse {
+
+    private Integer mealTimeId;
+
+    private MealType mealType;
+
+    private List<MealFoodResponse> foods;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public static MealTimeResponse toDto(MealTime mealTime) {
+        return MealTimeResponse.builder()
+                .mealTimeId(mealTime.getMealTimeId())
+                .mealType(mealTime.getMealType())
+                .foods(mealTime.getMealFoods().stream()
+                        .map(MealFoodResponse::toDto)
+                        .collect(Collectors.toList()))
+                .createdAt(mealTime.getCreatedAt())
+                .updatedAt(mealTime.getUpdatedAt())
+                .build();
+    }
+}
