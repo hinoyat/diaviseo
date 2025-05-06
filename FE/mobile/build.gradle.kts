500f80d3383610a6b658b6fb44ef2a790a2e573b
임채현 <chi_927@naver.com>
2025-05-07 01:25:59 +0900

    [FE] Config: vico 버전 업그레이드

--- FE/mobile/build.gradle.kts
+++ FE/mobile/build.gradle.kts
@@ -90,8 +90,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
 
-    implementation("com.patrykandpatrick.vico:compose:1.13.0")
-    implementation("com.patrykandpatrick.vico:core:1.13.0")
+    implementation("com.patrykandpatrick.vico:compose:1.13.1")
+    implementation("com.patrykandpatrick.vico:core:1.13.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
