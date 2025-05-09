91a70e7dfab0026d4f3d01621677a3dcbeb28065
김성현 <aruesin2@gmail.com>
2025-05-09 21:34:37 +0900

    [BE] Feat: 알림 서비스 유레카 등록
    - @SpringBootApplication 및 메인 메소드 추가
    - @EnableDiscoveryClient로 Eureka 서버에 서비스 등록 설정
    - 알림 서비스 컴포넌트 스캔 설정 (@ComponentScan)

--- BE/alert-service/src/main/java/com/s206/alert/AlertApplication.java
+++ BE/alert-service/src/main/java/com/s206/alert/AlertApplication.java
@@ -2,8 +2,12 @@ package com.s206.alert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.s206.alert", "com.s206.common"})
 public class AlertApplication {
 
 	public static void main(String[] args) {
