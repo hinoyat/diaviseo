9b1bed23741d7cf7387539735f00232d89efedd0
이다은 <danivalen@naver.com>
2025-05-10 17:36:12 +0900

    [FE] feat : 운동 등록 Api 서비스 파일 개설

---
+++ FE/mobile/src/main/java/com/example/diaviseo/network/exercise/ExerciseApiService.kt
@@ -0,0 +1,16 @@
+package com.example.diaviseo.network.exercise
+
+import com.example.diaviseo.network.exercise.dto.req.ExerciseRecordRequest
+import com.example.diaviseo.network.common.dto.ApiResponse
+import com.example.diaviseo.network.exercise.dto.res.ExerciseRecordResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ExerciseApiService {
+
+    // 운동 등록
+    @POST("exercises")
+    suspend fun registerExercise(
+        @Body request: ExerciseRecordRequest
+    ): ApiResponse<ExerciseRecordResponse>
+}
