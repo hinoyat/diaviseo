1653264fe37f56faa16361ca0cf8ba61d47bc66b
임채현 <chi_927@naver.com>
2025-05-11 02:03:02 +0900

    [FE] Feat: 일일 운동 조회 api

--- FE/mobile/src/main/java/com/example/diaviseo/network/exercise/ExerciseApiService.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/network/exercise/ExerciseApiService.kt
@@ -3,14 +3,22 @@ package com.example.diaviseo.network.exercise
 import com.example.diaviseo.network.exercise.dto.req.ExerciseRecordRequest
 import com.example.diaviseo.network.common.dto.ApiResponse
 import com.example.diaviseo.network.exercise.dto.res.ExerciseRecordResponse
+import com.example.diaviseo.network.exercise.dto.res.DayExerciseStatsResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ExerciseApiService {
-
     // 운동 등록
     @POST("exercises")
     suspend fun registerExercise(
         @Body request: ExerciseRecordRequest
     ): ApiResponse<ExerciseRecordResponse>
+
+    // 날짜로 해당날 운동 조회
+    @GET("exercises/today")
+    suspend fun fetchDailyExercise(
+        @Query("date") date: String
+    ): ApiResponse<DayExerciseStatsResponse>
 }
