cd84be3bd05852e1fc516b4e6198d488027df770
임채현 <chi_927@naver.com>
2025-05-11 21:14:03 +0900

    [FE] Feat: 식단정보 존재할 때 보일 상세페이지의 카드부분

--- FE/mobile/src/main/java/com/example/diaviseo/ui/detail/MealDetailScreen.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/detail/MealDetailScreen.kt
@@ -16,15 +16,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.diaviseo.ui.components.CommonTopBar
 import com.example.diaviseo.ui.components.DiaDatePickerDialog
 import com.example.diaviseo.ui.components.LoadingOverlay
+import com.example.diaviseo.ui.detail.components.meal.MealCard
 import com.example.diaviseo.ui.main.components.goal.DonutChartWithLegend
 import com.example.diaviseo.viewmodel.GoalViewModel
 import com.example.diaviseo.viewmodel.ProfileViewModel
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.geometry.Offset
+import com.example.diaviseo.network.meal.dto.res.MealFoodResponse
+import com.example.diaviseo.network.meal.dto.res.MealNutritionResponse
 
 @SuppressLint("UnrememberedGetBackStackEntry")
 @Composable
@@ -72,6 +78,64 @@ fun MealDetailScreen(
     // 몇월 며칠 날짜 파싱
     val day = selectedDate.dayOfMonth.toString()
 
+    val dummyBrush = Brush.linearGradient(
+        0.0f to Color.Red,
+        0.3f to Color.Green,
+        1.0f to Color.Blue,
+        start = Offset(0.0f, 50.0f),
+        end = Offset(0.0f, 100.0f)
+    )
+
+    val dummyNutrition = MealNutritionResponse(
+        totalCalorie = 910,
+        totalCarbohydrate = 128.00,
+        totalProtein = 72.80,
+        totalFat = 11.60,
+        totalSugar = 0.60,
+        totalSodium = 150.00
+    )
+
+    val dummyFood: List<MealFoodResponse> = listOf(
+        MealFoodResponse(
+            mealFoodId = 10,
+            foodId = 2,
+            foodName = "닭가슴살",
+            calorie = 165,
+            carbohydrate = 0.00,
+            protein = 31.00,
+            fat = 3.60,
+            sugar = 0.00,
+            sodium = 70.00,
+            quantity = 2,
+            foodImageUrl = "https://example.com/image1.jpg",
+            totalCalorie = 330,
+            totalCarbohydrate = 0.00,
+            totalProtein = 62.00,
+            totalFat = 7.20,
+            totalSugar = 0.00,
+            totalSodium = 140.00
+        ),
+        MealFoodResponse(
+            mealFoodId = 11,
+            foodId = 1,
+            foodName = "현미밥",
+            calorie = 290,
+            carbohydrate = 64.00,
+            protein = 5.40,
+            fat = 2.20,
+            sugar = 0.30,
+            sodium = 5.00,
+            quantity = 2,
+            foodImageUrl = "https://example.com/image1.jpg",
+            totalCalorie = 580,
+            totalCarbohydrate = 128.00,
+            totalProtein = 10.80,
+            totalFat = 4.40,
+            totalSugar = 0.60,
+            totalSodium = 10.00
+        )
+    )
+
     Scaffold(
         topBar = {
             CommonTopBar(
@@ -98,6 +162,17 @@ fun MealDetailScreen(
                 sugarRatio = 0.1f,
                 proteinRatio = 0.1f,
                 fatRatio = 0.2f
+            )
+
+            MealCard(
+                title = "아침",
+                kcal = 330,
+                eatingTime = "08:30:00",
+                nutrition = dummyNutrition,
+                foods = dummyFood,
+                gradient = dummyBrush,
+                mealIconRes = "BREAKFAST",
+                onEditClick = {}
             )
         }
 
