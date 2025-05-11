e326234bd1adba31799251a12138e7a98af2ebed
이다은 <danivalen@naver.com>
2025-05-11 10:20:29 +0900

    [FE] fix : 운동 번호 res,req 컬럼명 변경

--- FE/mobile/src/main/java/com/example/diaviseo/network/exercise/dto/req/ExerciseRecordRequest.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/network/exercise/dto/req/ExerciseRecordRequest.kt
@@ -1,7 +1,7 @@
 package com.example.diaviseo.network.exercise.dto.req
 
 data class ExerciseRecordRequest(
-    val exerciseTypeId: Int,      // 운동 종류 ID (필수)
+    val exerciseNumber: Int,      // 운동 종류 넘버
     val exerciseDate: String?,    // 운동 시작 시간 (예: "2025-05-10T08:30:00"), null 가능
     val exerciseTime: Int         // 운동 시간 (단위: 분)
 )
