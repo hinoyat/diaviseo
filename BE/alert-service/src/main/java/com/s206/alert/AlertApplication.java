ad8affe9b9638d594ba9b609994cffe8919dbfc5
김성현 <aruesin2@gmail.com>
2025-05-12 23:51:14 +0900

    [BE] Feat: AlertApplication.java @EnableFeignClients 추가

--- BE/alert-service/src/main/java/com/s206/alert/AlertApplication.java
+++ BE/alert-service/src/main/java/com/s206/alert/AlertApplication.java
@@ -3,11 +3,13 @@ package com.s206.alert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.s206.alert", "com.s206.common"})
+@EnableFeignClients(basePackages = {"com.s206.alert.client"})
 public class AlertApplication {
 
 	public static void main(String[] args) {
