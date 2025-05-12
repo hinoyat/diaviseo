94a8d774923f22d9ca69002bdb0c28058e5fc859
이다은 <danivalen@naver.com>
2025-05-12 17:05:43 +0900

    [FE] feat: 음식 세트 목록 렌더링용 FoodSetList 컴포넌트 추가

---
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/register/diet/components/FoodSetList.kt
@@ -0,0 +1,42 @@
+package com.example.diaviseo.ui.register.diet.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.diaviseo.network.foodset.dto.res.FoodSetResponse
+
+
+@Composable
+fun FoodSetList(
+    foodSets: List<FoodSetResponse>,
+    onSetClick: (FoodSetResponse) -> Unit = {},
+    onAddSetClick: () -> Unit = {}
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(
+            start = 22.dp,
+            end = 22.dp,
+            top = 8.dp,
+            bottom = 150.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(foodSets) { foodSet ->
+            val foodList = foodSet.foods.map { it.foodName to it.calorie }
+
+            FoodSetCard(
+                title = foodSet.name,
+                foodList = foodList,
+                onClickRecord = { onSetClick(foodSet) }
+            )
+        }
+        item {
+            AddFoodSetCard(onClick = onAddSetClick)
+        }
+    }
+}
