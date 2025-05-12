c980b3a2017f07787f89272979afd5eb1e96e1ac
김성현 <aruesin2@gmail.com>
2025-05-12 23:48:23 +0900

    [BE] Feat: 알림 대상 User 목록 반환 API 구현

---
+++ BE/alert-service/src/main/java/com/s206/alert/client/dto/response/UserDetailResponse.java
@@ -0,0 +1,46 @@
+package com.s206.alert.client.dto.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailResponse {
+
+	private String name;
+
+	private String nickname;
+
+	private Gender gender;
+
+	private BigDecimal height;
+
+	private BigDecimal weight;
+
+	private Goal goal;
+
+	private LocalDate birthday;
+
+	private String phone;
+
+	private String email;
+
+	private Boolean consentPersonal;
+
+	private Boolean locationPersonal;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	private LocalDateTime deletedAt;
+
+	private Boolean isDeleted;
+
+	private Boolean notificationEnabled;
+}
