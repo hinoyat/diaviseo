41c34cdac0a2a3339e640d780e910e6de39381db
김성현 <aruesin2@gmail.com>
2025-05-12 23:48:59 +0900

    [BE] Feat: 식단 등록한 유저 목록 반환하는 쿼리

--- BE/health-service/src/main/java/com/s206/health/nutrition/meal/repository/MealTimeRepository.java
+++ BE/health-service/src/main/java/com/s206/health/nutrition/meal/repository/MealTimeRepository.java
@@ -1,7 +1,24 @@
 package com.s206.health.nutrition.meal.repository;
 
 import com.s206.health.nutrition.meal.entity.MealTime;
+import com.s206.health.nutrition.meal.entity.MealType;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MealTimeRepository extends JpaRepository<MealTime, Integer> {
+
+	// 점심 미입력 유저 조회 쿼리
+	@Query("SELECT DISTINCT m.userId FROM Meal m JOIN m.mealTimes mt " +
+			"WHERE m.userId IN :userIds " +
+			"AND m.mealDate = :date " +
+			"AND mt.mealType = :mealType " +
+			"AND m.isDeleted = false " +
+			"AND mt.isDeleted = false")
+	List<Integer> findUserIdsWithMealTime(
+			@Param("userIds") List<Integer> userIds,
+			@Param("date") LocalDate date,
+			@Param("mealType") MealType mealType);
 }
