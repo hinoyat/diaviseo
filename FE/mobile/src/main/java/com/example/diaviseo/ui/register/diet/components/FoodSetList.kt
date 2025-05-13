daf9c2d06b115eb02654cd4aea98b24b440b7aa9
이다은 <danivalen@naver.com>
2025-05-13 16:34:31 +0900

    [FE] fix : 목록이 비어있는 경우 분기, UI 연결

--- FE/mobile/src/main/java/com/example/diaviseo/ui/register/diet/components/FoodSetList.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/register/diet/components/FoodSetList.kt
@@ -3,11 +3,17 @@ package com.example.diaviseo.ui.register.diet.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.diaviseo.network.foodset.dto.res.FoodSetResponse
-
+import androidx.compose.ui.graphics.Color
+import com.example.diaviseo.ui.theme.medium13
 
 @Composable
 fun FoodSetList(
@@ -15,27 +21,57 @@ fun FoodSetList(
     onSetClick: (FoodSetResponse) -> Unit = {},
     onAddSetClick: () -> Unit = {}
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(
-            start = 22.dp,
-            end = 22.dp,
-            top = 8.dp,
-            bottom = 150.dp
-        ),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        items(foodSets) { foodSet ->
-            val foodList = foodSet.foods.map { it.foodName to it.calorie }
+    if (foodSets.isEmpty()) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            color = Color.White
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // EmptyStateView 포함
+                EmptyStateView(tabType = EmptyTabType.FOOD_SET)
 
-            FoodSetCard(
-                title = foodSet.name,
-                foodList = foodList,
-                onClickRecord = { onSetClick(foodSet) }
-            )
+                // 등록 버튼 동일한 위치
+                TextButton(onClick = onAddSetClick) {
+                    Text(
+                        text = "등록하러가기",
+                        style = medium13,
+                        color = Color(0xFF96BEFF),
+                    )
+                }
+            }
+
         }
-        item {
-            AddFoodSetCard(onClick = onAddSetClick)
+
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(
+                start = 22.dp,
+                end = 22.dp,
+                top = 8.dp,
+                bottom = 150.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(foodSets) { foodSet ->
+                val foodList = foodSet.foods.map { it.foodName to it.calorie }
+
+                FoodSetCard(
+                    title = foodSet.name,
+                    foodList = foodList,
+                    onClickRecord = { onSetClick(foodSet) }
+                )
+            }
+            item {
+                AddFoodSetCard(onClick = onAddSetClick)
+            }
         }
     }
 }
