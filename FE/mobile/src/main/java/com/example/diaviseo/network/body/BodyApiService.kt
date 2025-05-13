0009a6fe482264359ebadf5ee25ff3bafa87d1fc
이다은 <danivalen@naver.com>
2025-05-13 22:26:19 +0900

    [FE] feat : 인바디 전송 로직 api 등록

--- FE/mobile/src/main/java/com/example/diaviseo/network/body/BodyApiService.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/network/body/BodyApiService.kt
@@ -2,13 +2,23 @@ package com.example.diaviseo.network.body
 
 import com.example.diaviseo.network.body.dto.req.BodyRegisterRequest
 import com.example.diaviseo.network.body.dto.res.BodyRegisterResponse
+import com.example.diaviseo.network.body.dto.res.OcrBodyResultResponse
 import com.example.diaviseo.network.common.dto.ApiResponse
+import okhttp3.MultipartBody
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface BodyApiService {
     @POST("bodies")
     suspend fun registerBodyData(
         @Body request: BodyRegisterRequest
     ): ApiResponse<BodyRegisterResponse>
+
+    @Multipart
+    @POST("bodies/ocr")
+    suspend fun uploadBodyOcrImage(
+        @Part image: MultipartBody.Part
+    ): ApiResponse<OcrBodyResultResponse>
 }
