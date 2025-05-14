398cacc6448b341567458ebfe4e3393e17f9c218
이다은 <danivalen@naver.com>
2025-05-14 13:37:13 +0900

    [FE] fix : 최근 먹은 음식 탭 타이틀 추가

--- FE/mobile/src/main/java/com/example/diaviseo/ui/register/diet/components/RecentFoodList.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/ui/register/diet/components/RecentFoodList.kt
@@ -3,16 +3,14 @@ package com.example.diaviseo.ui.register.diet.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.shape.RoundedCornerShape
 import com.example.diaviseo.mapper.toFoodItem
 import com.example.diaviseo.network.food.dto.res.FoodItem
 import com.example.diaviseo.network.food.dto.res.RecentFoodItemResponse
-import com.example.diaviseo.ui.components.AddRemoveIconButton
+import com.example.diaviseo.ui.theme.*
+import com.example.diaviseo.ui.theme.DiaViseoColors
 
 @Composable
 fun RecentFoodList(
@@ -20,7 +18,8 @@ fun RecentFoodList(
     selectedItems: List<Int>,
     fetchedDate: String?,
     onToggleSelect: (FoodItem) -> Unit,
-    onFoodClick: (FoodItem) -> Unit
+    onFoodClick: (FoodItem) -> Unit,
+    nickname: String
 ) {
     val mappedFoods = foods.map { it.toFoodItem() }
 
@@ -29,13 +28,25 @@ fun RecentFoodList(
             EmptyStateView(tabType = EmptyTabType.RECENT_FOOD)
         } else {
             fetchedDate?.let {
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Text(
+                    text = "$nickname 님이 최근 드신 음식 목록",
+                    style = bold17,
+                    color = Color.Black,
+                    modifier = Modifier.padding(start = 3.dp, bottom = 4.dp)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+
                 Text(
                     text = "${it.replace("-", ".")} 기준",
-                    style = MaterialTheme.typography.bodySmall,
-                    color = Color.Gray,
-                    modifier = Modifier.padding(start = 12.dp, bottom = 8.dp)
+                    style = regular14,
+                    color = DiaViseoColors.Placeholder,
+                    modifier = Modifier
+                        .padding(start = 3.dp, bottom = 8.dp)
                 )
             }
+            Spacer(modifier = Modifier.height(5.dp))
 
             SearchSuggestionList(
                 results = mappedFoods,
