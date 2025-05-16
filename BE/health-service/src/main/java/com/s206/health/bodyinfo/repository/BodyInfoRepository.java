2d83cbd04b763d0ea5749cb750dd365e7132bb30
강현호 <hhwj2270@gmail.com>
2025-05-16 11:16:38 +0900

    [BE] Feat: date 계산 쿼리 수정

--- BE/health-service/src/main/java/com/s206/health/bodyinfo/repository/BodyInfoRepository.java
+++ BE/health-service/src/main/java/com/s206/health/bodyinfo/repository/BodyInfoRepository.java
@@ -15,9 +15,10 @@ public interface BodyInfoRepository extends JpaRepository<BodyInfo, Integer> {
 
 	Optional<BodyInfo> findByBodyIdAndIsDeletedFalse(Integer bodyId);
 
-	@Query(value = "SELECT * FROM body_tb b WHERE b.user_id = :userId AND b.measurement_date = :date AND b.is_deleted = false ORDER BY b.created_at DESC LIMIT 1", nativeQuery = true)
+	@Query("SELECT b FROM BodyInfo b WHERE b.userId = :userId AND b.measurementDate = :date AND b.isDeleted = false ORDER BY b.createdAt DESC")
 	Optional<BodyInfo> findLatestBodyInfoByMeasurementDate(@Param("userId") Integer userId,
-			@Param("date") LocalDate date);
+														   @Param("date") LocalDate date);
+
 
 	@Query("SELECT b.measurementDate AS measurementDate, " +
 			"b.weight AS weight, " +
