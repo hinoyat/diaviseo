41cc6a986c95d2a3b1322531d81494cdcfd80a48
강현호 <hhwj2270@gmail.com>
2025-05-16 15:55:47 +0900

    [FE] Remove: AI 채팅 관련 엔드포인트 추가

--- FE/mobile/src/main/java/com/example/diaviseo/network/chatbot/ChatBotApiService.kt
+++ FE/mobile/src/main/java/com/example/diaviseo/network/chatbot/ChatBotApiService.kt
@@ -1,1 +1,44 @@
 package com.example.diaviseo.network.chatbot
+
+import com.example.diaviseo.network.chatbot.dto.req.ChatRequest
+import com.example.diaviseo.network.chatbot.dto.req.EndSessionRequest
+import com.example.diaviseo.network.chatbot.dto.req.StartSessionRequest
+import com.example.diaviseo.network.chatbot.dto.res.ChatMessageResponse
+import com.example.diaviseo.network.chatbot.dto.res.ChatResponse
+import com.example.diaviseo.network.chatbot.dto.res.EndSessionResponse
+import com.example.diaviseo.network.chatbot.dto.res.SessionSummaryResponse
+import com.example.diaviseo.network.common.dto.ApiResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+interface ChatBotApiService {
+    @POST("chatbot/start-session")
+    suspend fun startSession(
+        @Body request: StartSessionRequest
+    ):ApiResponse<StartSessionRequest>
+
+
+    @POST("chatbot/chat/{sessionId}")
+    suspend fun sendChatMessage(
+        @Path("sessionId") sessionId: String,
+        @Body request: ChatRequest
+    ): ChatResponse
+
+    @GET("chatbot/chats")
+    suspend fun getChatSessions(): List<SessionSummaryResponse>
+
+    @GET("chatbot/messages/{sessionId}")
+    suspend fun getChatMessages(
+        @Path("sessionId") sessionId: String
+    ): List<ChatMessageResponse>
+
+    @POST("chatbot/end-session")
+    suspend fun endSession(
+        @Body request: EndSessionRequest
+    ): EndSessionResponse
+
+
+}
