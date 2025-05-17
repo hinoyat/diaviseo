0206745cc9fe67c73dd23a474479634e3e80c1ca
seorinKim <seorin66@naver.com>
2025-05-17 17:29:29 +0900

    [AI] Chore: 식단 앤드포인트 수정중완료(memory 보완필요)

---
+++ AI/app/services/nutrition/nutrition_chat_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+from app.services.memory.mongo_memory import get_memory
+from app.core.T5_wrapping import get_t5_langchain_llm
+from langchain.prompts import PromptTemplate
+from langchain.chains.conversation.base import ConversationChain
+
+def generate_nutrition_response(user_input:str, session_id:str, user_db:Session, user_id: int):
+    print("✅ generate_nutrition_response 호출됨")
+    memory = get_memory(session_id)
+    llm = get_t5_langchain_llm()
+    response = llm.invoke(input=user_input)
+
+    return response
+
+# def is_uncertain(user_input:str)
+#
+#     return response
