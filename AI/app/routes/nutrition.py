33ca10bbe8248f2e05489e4a63bbd4928a946e49
seorinKim <seorin66@naver.com>
2025-05-19 09:41:54 +0900

    [AI] Feat: 욜로를 통한 음식영양정보 조회 기능 구현

--- AI/app/routes/nutrition.py
+++ AI/app/routes/nutrition.py
@@ -6,8 +6,9 @@ from sqlalchemy.orm import Session
 from app.db.mysql import get_session
 from app.repositories.feedback_repository import insert_feedback
 from app.services.nutrition.nutrition_chat_service import generate_nutrition_response
-from app.schemas.nutrition import ChatRequest
+from app.schemas.nutrition import ChatRequest, FoodNutritionResponse, FoodRequest
 from app.services.nutrition.feedback_service import generate_nutrition_feedback
+from app.services.nutrition.get_food_nutrition_service import get_food_nutrition
 router = APIRouter()
 
 @router.post("/nutrition_chat")
@@ -31,3 +32,9 @@ def nutrition_feedback(datetime: date, user_db:Session = Depends(get_session("us
     insert_feedback(user_id=user_id,feedback=response,feedback_type=FeedbackType.nutrition )
 
     return {"answer": response}
+
+
+@router.post("/nutrition_food", response_model=FoodNutritionResponse)
+def nutrition_food(request: FoodRequest, db: Session = Depends(get_session("health"))):
+
+    return get_food_nutrition(request.foodName, db)
